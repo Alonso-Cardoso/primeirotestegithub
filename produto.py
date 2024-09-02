af60499102445dd6aaa22bac63d7e6589dc225fb
Alonso-Cardoso <alonsocardoso@gmail.com>
2024-09-01 21:28:58 -0300

    Script Completo

--- produto.py
+++ produto.py
@@ -54,7 +54,7 @@ class ProdutoVenda(Produto):
     def __str__(self):
         texto = super().__str__()
         texto += ', Quantidade: {q:0.3f}'.format(q = self._quantidade)
-        texto += ', Total: R${t:0.2f}'.format(t = self.total())
+        texto += ', Total: R${t:0.2f}'.format(t = self.total)
         return texto
 
 class Venda:
@@ -70,7 +70,7 @@ class Venda:
     def numero_produtos(self):
         return len(self._produtos)
     
-    def adicona_produto(self,produto):
+    def adiciona_produto(self,produto):
         self._produtos.append(produto)
         self._total += produto.total
 
@@ -83,8 +83,125 @@ class Venda:
         texto += 'Total da venda: {t:0.2f}'.format(t=self._total)
         texto += '\n' + '-'*50
         return texto
-        
+    
+def pergunta(mensagem,tipo=int):
+    while True:
+        try:
+            resp = input(mensagem)
+            return tipo(resp)
+        except:
+            print('Valor Inválido!! Informe Novamente!')
+
+def confirma(mensagem,resposta=str):
+    texto = input(mensagem).lower().split()
+    resp = resposta.lower()
+    if resp in texto:
+        return True
+    return False
+
+class Caixa:
+    def __init__(self):
+        self._produtos = {}
+        self._vendas = []
+
+    def menu(cls):
+        print()
+        print('********************************')
+        print('* CAIXA *')
+        print('********************************')
+        print('(C) Cadastrar/atualizar produto ')
+        print('(E) Entrada de estoque ')
+        print('(V) Vender ')
+        print('(R) Relatório de vendas ')
+        print('(S) Sair ')
+        print('********************************')
+        escolha = input('Informe sua opção: ').upper()
+        return escolha
+
+    def busca_produto(self):
+        if len(self._produtos) == 0:
+            print('Nenhum produto cadastrado!!')
+            return None
+        for cod, produto in self._produtos.items():
+            print(cod, ':',produto)
+        codigo = pergunta('Código do produto:')
+        if codigo in self._produtos:
+            return self._produtos[codigo]
+        print('Produto não encontrado!!')
+        return None
+    
+    @classmethod
+    def dados_produto(cls):
+        print('Informe os dados: ')
+        descricao = input('Descrição do produto: ')
+        preco = pergunta('Preço',float)
+        return descricao, preco
+    
+    def cadastro_produto(self):
+        produto = self.busca_produto()
+        if produto is not None:
+            print('Produto Cadastrado: ', produto)
+            if confirma('Alterar? (S/N)', 'S'):
+                descricao, preco = self.dados_produto()
+                produto.descricao = descricao
+                produto.preco = preco
+
+        else:
+            if confirma('Incluir? (S/N)', 'S'):
+                descricao, preco = self.dados_produto()
+                produto = ProdutoEstoque(descricao, preco)
+                codigo = len(self._produtos)
+                self._produtos[codigo] = produto
+
+    def entrada_estoque(self):
+        produto = self.busca_produto()
+        if produto is not None:
+            quantidade = pergunta('Quantidade de entrada: ', float)
+            produto.entrada(quantidade)
+
+    def venda(self):
+            print('Venda')
+            venda = Venda()
+            while True:
+                produto = self.busca_produto()
+                if produto is not None:
+                    quantidade = pergunta('Quantidade vendida:', float)
+                    if produto.saida(quantidade):
+                        produto_venda = ProdutoVenda(produto.descricao,produto.preco,quantidade)
+                        venda.adiciona_produto(produto_venda)
+                print(venda)
+                if confirma('Adicionar mais produtos: (S/N)', 'N'):
+                    break
+            if venda.numero_produtos > 0:
+                self._vendas.append(venda)
+    
+    def relatorio_vendas(self):
+        if len(self._vendas) == 0:
+            print('Nenhuma venda efetuada')
+            return
+        total_geral = 0
+        for cont, venda in enumerate(self._vendas):
+            print('\nVenda ',cont)
+            print(venda)
+            total_geral += venda.total
+            print('Total Geral: ', total_geral)            
+            input('Pressione "ENTER" para voltar.')
+
+    def iniciar(self):
+        while True:
+            escolha = self.menu()
+            if escolha == 'C':
+                self.cadastro_produto()
+            elif escolha == 'E':
+                self.entrada_estoque()
+            elif escolha == 'V':
+                self.venda()
+            elif escolha == 'R':
+                self.relatorio_vendas()
+            elif escolha == 'S':
+                break
 
 
-prod = ProdutoEstoque('Arroz',12.5)
-print (prod.__str__())
+if __name__ == '__main__':
+    Caixa().iniciar()
+ 
